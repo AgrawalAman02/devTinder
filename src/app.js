7afb239497e2fb8989d0d69812a9bb0268b00d75
AgrawalAman02 <amanhzb2003@gmail.com>
2024-09-29 14:08:47 +0530

    exploring GET, POST, DEL

--- src/app.js
+++ src/app.js
@@ -28,22 +28,48 @@ app.use("/",(req,res)=>{
 */
 // if this piece of code is written below test then this abraadabrah will not print in this route as fisrst it check the /test is availbale ? -Yes then it will print 
 // but we are writing before then forst it check the /test/1 if yess then it send this response otherwise / test response
-app.use("/test/1", (req,res)=>{
-    res.send("ABRAKADABRAH...");    
-});
+// app.use("/test/1", (req,res)=>{
+//     res.send("ABRAKADABRAH...");    
+// });
 
-app.use("/test",(req,res)=>{
-    res.send("hello from test");
-});
-// but on normal localhost:7777 we will get msg as "Cannot GET /"  as we define for /test 
-// but when we write /home then we get "cannot GET /home" lets define  for /home..
-app.use("/home",(req,res)=>{
-    res.send("hello from home page");
-});
+// app.use("/test",(req,res)=>{
+//     res.send("hello from test");
+// });
+// // but on normal localhost:7777 we will get msg as "Cannot GET /"  as we define for /test 
+// // but when we write /home then we get "cannot GET /home" lets define  for /home..
+// app.use("/home",(req,res)=>{
+//     res.send("hello from home page");
+// });
 
-app.use("/",(req,res)=>{
-    res.send("Namaste from server");
-});
+// app.use("/",(req,res)=>{
+//     res.send("Namaste from server");
+// });
 
 // ! Note : we dont want to rerun again again so we use nodemon & we dont want o write long command again and again so we write it in in the package.json 
 // we only need npm run dev 
+
+// !---------------------------POST----------------------------------! //
+
+/*
+? as we are using the app.use() so it is matching all the http methods 
+* but if we do the app.get then it only match the GET method of http
+
+*/
+
+app.get("/user", (req,res)=>{
+    res.send({firstName: "Aman", lastName: "Agrawal" });
+})
+// we cant get respone when we do post call to the /user 
+// but if we use app.post then we can make post call
+app.post("/user",(req,res)=>{
+    // "save data to the database"
+    res.send("Data had beeen successfully saved to the database");
+})
+
+// we can also make delete call
+app.delete("/user", (req,res)=>{
+    res.send("data deleted successfully");
+});
+app.use("/",(req,res)=>{
+    res.send("Namaste from server");
+});
