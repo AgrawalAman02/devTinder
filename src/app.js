7bcdd73b77c6b3eaa9949433206ec5e5407b9712
AgrawalAman02 <amanhzb2003@gmail.com>
2024-10-22 16:20:56 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -56,6 +56,7 @@ app.use("/",(req,res)=>{
 
 */
 
+/*
 app.get("/user", (req,res)=>{
     res.send({firstName: "Aman", lastName: "Agrawal" });
 })
@@ -70,6 +71,66 @@ app.post("/user",(req,res)=>{
 app.delete("/user", (req,res)=>{
     res.send("data deleted successfully");
 });
+
+*/
+
+//! Date 22/10/2024
+
+// now let learn some more  Advanced routing technique
+
+// app.get("/abc",(req,res)=>{
+//     res.send("Checked Successfully!...");
+// });
+// now if we use the <char>? then that char is optional 
+
+// app.get("/ab?c",(req,res)=>{  // b is optional...
+//     res.send("Checked Successfully!...");
+// });
+
+// now on having <char>+ then we can use any no. of that char(atleast one) , then still our response is same 
+// app.get("/ab+c",(req,res)=>{  // any no. of b then same response  
+//     res.send("Checked Successfully!...");
+// });
+
+// now if we use <char>* then at the position of * we can insert any string then stillour result is same 
+// app.get("/ab*c",(req,res)=>{  // b is optional...
+//     res.send("Checked Successfully!...");
+// });
+
+// we can even grp the string using the ();
+// app.get("/a(bcd)?c",(req,res)=>{  // bcd as a grp is optional...
+//     res.send("Checked Successfully!...");
+// });
+
+// ? we can even write regex there 
+// the regex means that if that condition occurs then the response we will abke to get;
+// app.get(/a/,(req,res)=>{  // anywhere if we get the a then response ...
+//     res.send("Checked Successfully!...");
+// });
+// we can also write comples reges tooo
+// like /.*fly$/ -> means that * = means starts with anything . so if the path starts with anything and end with fly then the response we get
+// buttterfly , mutterfly, jfoefly,etc......
+// app.get(/.*fly$/,(req,res)=>{  
+//     res.send("Checked Successfully!...");
+// }); 
+
+
+// // we an even pass dynamic userid and we can also get that params/query
+// app.get("/user",(req,res)=>{     // userid is dynamic in the webpage like user?userid=101 and we can even send more thing by using & like   http://localhost:7777/user?userid=106&pwd=agr
+//     // we can get that query params also-> in req.query
+//     console.log(req.query);  // this print that id in json format in the console   -> { userid: '106', pwd: 'agr' }
+//     res.send("Checked Successfully!...");
+// });
+    
+// now how can we handle dynamic apis like /user/121 , /user/2 
+// we can user /:userId then anything after / will save in the userId and we can print it by req.params
+app.get("/user/:userId/:name",(req,res)=>{ 
+    console.log(req.params);
+    res.send("Checked Successfully!...");
+});  // like http://localhost:7777/user/102/aman then we got { userId: '102', name: 'aman' }
+
+
+
 app.use("/",(req,res)=>{
     res.send("Namaste from server");
 });
